dc38552062423df87d31f885953693462e1c5510
simonegarofalo <simonegarofalo96@gmail.com>
2024-12-04 15:55:45 +0100

    Added GSAP (and ScrollTrigger) functions on scroll

--- script.js
+++ script.js
@@ -8,3 +8,16 @@ textElement.addEventListener('animationiteration', () => {
   textIndex = (textIndex + 1) % texts.length;
   textElement.textContent = texts[textIndex];
 });
+
+gsap.set("#card-1", { y: 100, opacity: 0 });
+ScrollTrigger.batch("#card-1", {
+  onEnter: (batch) => gsap.to(batch, { opacity: 1, y: 0, stagger: 0.5 }),
+});
+gsap.set("#card-2", { y: 100, opacity: 0 });
+ScrollTrigger.batch("#card-2", {
+  onEnter: (batch) => gsap.to(batch, { opacity: 1, y: 0, stagger: 0.5 }),
+});
+gsap.set("#card-3", { y: 100, opacity: 0 });
+ScrollTrigger.batch("#card-3", {
+  onEnter: (batch) => gsap.to(batch, { opacity: 1, y: 0, stagger: 0.5 }),
+});
